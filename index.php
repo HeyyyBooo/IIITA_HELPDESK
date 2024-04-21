0f77a96688c6c414de2e3858dd2fc1b2062cdf1f
HeyyyBooo <154744741+HeyyyBooo@users.noreply.github.com>
2024-04-22 00:07:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
   <title>HelpDesk IIITA</title>
   <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  <link rel="icon" href="assets/icon.gif" type="image/x-icon">
+  <link rel="shortcut icon" href="assets/icon.gif" type="image/x-icon">
   <link rel="stylesheet" href="./style.css">
   <style>
     body {
